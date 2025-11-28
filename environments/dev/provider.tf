79c497ea2504b2008d528709c6de2585dcd9e201
Sohitgupta1991 <sohitg89@gmail.com>
2025-11-29 03:33:19 +0530

    updated

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -7,13 +7,16 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "sohit-docker-vm-dont-delete"
-    storage_account_name = "sohitstg"
+    storage_account_name = "sohitstg1"
     container_name       = "sohittfstate"
     key                  = "dev.tfstate"
+    tenant_id            = "ea9ca8b6-aa2d-4c4a-904a-cb93fd58a0b9"
+    subscription_id      = "86cd11f5-4d26-4e93-bfa9-c0b7b2f44cc9"
+
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "3130fe23-dc28-48ba-a8e3-43702c2970a2"
+  subscription_id = "86cd11f5-4d26-4e93-bfa9-c0b7b2f44cc9"
 }
